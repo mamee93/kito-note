ae057c5612e9c04c16c932cea1123cca5f626a39
maram <mameeal@gmail.com>
2020-11-11 22:01:49 +0400

    edit settings

--- project/settings.py
+++ project/settings.py
@@ -48,12 +48,14 @@ LOGOUT_REDIRECT_URL = '/accounts/login'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]  
 
 ROOT_URLCONF = 'project.urls'
